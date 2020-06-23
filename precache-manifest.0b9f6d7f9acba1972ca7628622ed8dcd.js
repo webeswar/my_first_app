aa0fefdc77d3ff3f3624ae6faf0c64fbe15cc8bb
webeswar <webeswar.1@gmail.com>
2020-06-23 07:32:10 +0530

    Updates

---
+++ precache-manifest.0b9f6d7f9acba1972ca7628622ed8dcd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c95002408be51b0c62245bf17a6e7d8",
+    "url": "/my_first_app/index.html"
+  },
+  {
+    "revision": "15ef0b4033baa9d28abc",
+    "url": "/my_first_app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b82a33093fad24cf34b9",
+    "url": "/my_first_app/static/js/2.721ea2e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my_first_app/static/js/2.721ea2e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ef0b4033baa9d28abc",
+    "url": "/my_first_app/static/js/main.c4ef81d3.chunk.js"
+  },
+  {
+    "revision": "638998c0cc0524e5e10c",
+    "url": "/my_first_app/static/js/runtime-main.fc9aeb9a.js"
+  },
+  {
+    "revision": "b935d5ccd35ef90dfeb307b33d5e5e58",
+    "url": "/my_first_app/static/media/person.b935d5cc.svg"
+  },
+  {
+    "revision": "2f5efde6e3342d794f1ccd63d1eaba10",
+    "url": "/my_first_app/static/media/person1.2f5efde6.svg"
+  }
+]);
